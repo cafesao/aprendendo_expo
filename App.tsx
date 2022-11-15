808ac1b873932ba4a073d70787be1f5793476daf
cafesao <my@cafesao.net>
2022-11-14 21:25:59 -0300

    Test Import and tailwindcss

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
+import ViewTest from './src/ViewTest'
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-violet-500">
-      <Text className="text-center text-white text-xl">Hello Word!</Text>
+      <ViewTest />
       <StatusBar style="auto" />
     </View>
   )
---
+++ src/ViewTest.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+
+export default function ViewTest() {
+  return (
+    <View className="flex items-center justify-center bg-violet-400 border-2 border-slate-600 p-10">
+      <Text className="text-center text-white text-xl">To do 1</Text>
+      <Text className="text-center text-white text-xl">To do 1</Text>
+      <Text className="text-center text-white text-xl">To do 1</Text>
+      <Text className="text-center text-white text-xl">To do 1</Text>
+      <Text className="text-center text-white text-xl">To do 1</Text>
+    </View>
+  )
+}
