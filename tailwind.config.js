73418bccf14dd6739a2ad53b55f19c07f51753ed
cafesao <my@cafesao.net>
2022-11-15 17:38:23 -0300

    Feat: Add Colors custom

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@
 module.exports = {
   content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      'toDo-background': '#9875C3',
+      'toDo-task': '#C3B1E9',
+      'toDo-edit': '#47ADB4',
+      'toDo-delete': '#B44747',
+    },
     extend: {},
   },
   plugins: [],
